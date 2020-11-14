1c754ef52ed9f32682e845f99dbb7d7565bd6b85
Matteo Zamaria <matteozamaria@gmail.com>
2020-11-13 21:10:22 -0500

    Add functionality to enclose text within specific characters.

--- js/main.js
+++ js/main.js
@@ -2,31 +2,86 @@
 // determines whether or not it is within an input field or text area,
 // and based on the key pressed, encloses that selection within certain characters (brackets, quotations, etc). 
 
-function getTextSelection(e) {
-    let _text = "";
-    let _element = document.activeElement;
-    let _elementTagName = _element ? _element.tagName.toLocaleLowerCase() : null;
+var enclosingCharacters = ["'", '"', '`', '/', '\\', '|', '[', ']', '(', ')', '{', '}', '<', '>'];
 
-    if (e.keyCode===17) {
+// Look for the selected text when a key is pressed
+document.onkeydown = selectAndEncloseText;
+
+// Get the selected text and enclose it within the appropriate character
+function selectAndEncloseText(e) {
+    let _keyDown = e.key;
+    //Determine if the key pressed corresponds to an enclosing character
+    if (enclosingCharacters.includes(_keyDown)) {
+        let _activeElement = document.activeElement;
+        let _elementTagName = _activeElement ? _activeElement.tagName.toLocaleLowerCase() : null;
+
+        // Determine if the element selected can be input into
         if (
             (_elementTagName === "textarea") || 
             (_elementTagName === "input" &&
-            /^(?:text|search|password|tel|url)$/i.test(_element.type)) &&
-            (typeof _element.selectionStart == "number")
-        ) {
-            _text = _element.value.slice(_element.selectionStart, _element.selectionEnd);
+            /^(?:text|search|password|tel|url)$/i.test(_activeElement.type)) &&
+            (typeof _activeElement.selectionStart == "number")
+        ) {       
+            let _text = _activeElement.value.slice(_activeElement.selectionStart, _activeElement.selectionEnd);
             if (_text) {
-                _element.value = (
-                    _element.value.slice(0, _element.selectionStart) + 
-                    '"' + 
-                    _element.value.slice(_element.selectionStart, _element.selectionEnd) + 
-                    '"' + 
-                    _element.value.slice(_element.selectionEnd, _element.value.length)
-                );
+                // Disable the field to prevent the character from being added to the end
+                _activeElement.disabled = true;
+
+                switch(_keyDown) {
+                    default:
+                        _activeElement.value = (
+                            _activeElement.value.slice(0, _activeElement.selectionStart) + 
+                            _keyDown + 
+                            _text + 
+                            _keyDown + 
+                            _activeElement.value.slice(_activeElement.selectionEnd, _activeElement.value.length)
+                        );
+                        break;
+                    case '[':
+                    case ']':
+                        _activeElement.value = (
+                            _activeElement.value.slice(0, _activeElement.selectionStart) + 
+                            '[' + 
+                            _text + 
+                            ']' + 
+                            _activeElement.value.slice(_activeElement.selectionEnd, _activeElement.value.length)
+                        );
+                        break;
+                    case '(':
+                    case ')':
+                        _activeElement.value = (
+                            _activeElement.value.slice(0, _activeElement.selectionStart) + 
+                            '(' + 
+                            _text + 
+                            ')' + 
+                            _activeElement.value.slice(_activeElement.selectionEnd, _activeElement.value.length)
+                        );
+                        break;
+                    case '{':
+                    case '}':
+                        _activeElement.value = (
+                            _activeElement.value.slice(0, _activeElement.selectionStart) + 
+                            '{' + 
+                            _text + 
+                            '}' + 
+                            _activeElement.value.slice(_activeElement.selectionEnd, _activeElement.value.length)
+                        );
+                        break;
+                    case '<':
+                    case '>':
+                        _activeElement.value = (
+                            _activeElement.value.slice(0, _activeElement.selectionStart) + 
+                            '<' + 
+                            _text + 
+                            '>' + 
+                            _activeElement.value.slice(_activeElement.selectionEnd, _activeElement.value.length)
+                        );
+                        break;                    
+                }                
+                // Re-enable and reselect the field
+                _activeElement.disabled = false;
+                // _activeElement.focus();
             }
         }
     }
 }
-
-//Look for selected text when a key is pressed
-document.onkeydown = getTextSelection;
