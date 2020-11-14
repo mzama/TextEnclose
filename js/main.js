7b18d86485e12057a2bf600eca233ce0a8296772
Matteo Zamaria <matteozamaria@gmail.com>
2020-11-13 21:35:13 -0500

    Finish base functionality -- 'enclosing characters' will surround highlighted text, or complete empty space.

--- js/main.js
+++ js/main.js
@@ -4,11 +4,8 @@
 
 var enclosingCharacters = ["'", '"', '`', '/', '\\', '|', '[', ']', '(', ')', '{', '}', '<', '>'];
 
-// Look for the selected text when a key is pressed
-document.onkeydown = selectAndEncloseText;
-
-// Get the selected text and enclose it within the appropriate character
-function selectAndEncloseText(e) {
+// Look for the selected text when a key is pressed -- if its within an input field, enclose it within the appropriate character
+document.onkeydown = function(e) {
     let _keyDown = e.key;
     //Determine if the key pressed corresponds to an enclosing character
     if (enclosingCharacters.includes(_keyDown)) {
@@ -21,67 +18,73 @@ function selectAndEncloseText(e) {
             (_elementTagName === "input" &&
             /^(?:text|search|password|tel|url)$/i.test(_activeElement.type)) &&
             (typeof _activeElement.selectionStart == "number")
-        ) {       
-            let _text = _activeElement.value.slice(_activeElement.selectionStart, _activeElement.selectionEnd);
-            if (_text) {
-                // Disable the field to prevent the character from being added to the end
-                _activeElement.disabled = true;
+        ) {    
+            let _start = _activeElement.selectionStart;
+            let _end = _activeElement.selectionEnd;   
+            let _text = _activeElement.value.slice(_start, _end);
+            
+            // Disable the field to prevent the character from being added to the end
+            _activeElement.disabled = true;
 
-                switch(_keyDown) {
-                    default:
-                        _activeElement.value = (
-                            _activeElement.value.slice(0, _activeElement.selectionStart) + 
-                            _keyDown + 
-                            _text + 
-                            _keyDown + 
-                            _activeElement.value.slice(_activeElement.selectionEnd, _activeElement.value.length)
-                        );
-                        break;
-                    case '[':
-                    case ']':
-                        _activeElement.value = (
-                            _activeElement.value.slice(0, _activeElement.selectionStart) + 
-                            '[' + 
-                            _text + 
-                            ']' + 
-                            _activeElement.value.slice(_activeElement.selectionEnd, _activeElement.value.length)
-                        );
-                        break;
-                    case '(':
-                    case ')':
-                        _activeElement.value = (
-                            _activeElement.value.slice(0, _activeElement.selectionStart) + 
-                            '(' + 
-                            _text + 
-                            ')' + 
-                            _activeElement.value.slice(_activeElement.selectionEnd, _activeElement.value.length)
-                        );
-                        break;
-                    case '{':
-                    case '}':
-                        _activeElement.value = (
-                            _activeElement.value.slice(0, _activeElement.selectionStart) + 
-                            '{' + 
-                            _text + 
-                            '}' + 
-                            _activeElement.value.slice(_activeElement.selectionEnd, _activeElement.value.length)
-                        );
-                        break;
-                    case '<':
-                    case '>':
-                        _activeElement.value = (
-                            _activeElement.value.slice(0, _activeElement.selectionStart) + 
-                            '<' + 
-                            _text + 
-                            '>' + 
-                            _activeElement.value.slice(_activeElement.selectionEnd, _activeElement.value.length)
-                        );
-                        break;                    
-                }                
-                // Re-enable and reselect the field
-                _activeElement.disabled = false;
-                // _activeElement.focus();
-            }
+            switch(_keyDown) {
+                default:
+                    _activeElement.value = (
+                        _activeElement.value.slice(0, _start) + 
+                        _keyDown + 
+                        _text + 
+                        _keyDown + 
+                        _activeElement.value.slice(_end, _activeElement.value.length)
+                    );
+                    break;
+                case '[':
+                case ']':
+                    _activeElement.value = (
+                        _activeElement.value.slice(0, _start) + 
+                        '[' + 
+                        _text + 
+                        ']' + 
+                        _activeElement.value.slice(_end, _activeElement.value.length)
+                    );
+                    break;
+                case '(':
+                case ')':
+                    _activeElement.value = (
+                        _activeElement.value.slice(0, _start) + 
+                        '(' + 
+                        _text + 
+                        ')' + 
+                        _activeElement.value.slice(_end, _activeElement.value.length)
+                    );
+                    break;
+                case '{':
+                case '}':
+                    _activeElement.value = (
+                        _activeElement.value.slice(0, _start) + 
+                        '{' + 
+                        _text + 
+                        '}' + 
+                        _activeElement.value.slice(_end, _activeElement.value.length)
+                    );
+                    break;
+                case '<':
+                case '>':
+                    _activeElement.value = (
+                        _activeElement.value.slice(0, _start) + 
+                        '<' + 
+                        _text + 
+                        '>' + 
+                        _activeElement.value.slice(_end, _activeElement.value.length)
+                    );
+                    break;                    
+            }                
+            // Re-enable and reselect the field
+            _activeElement.disabled = false;
+            _activeElement.setSelectionRange(_start + 1, _end + 1);
+            //Call after a delay so the actual input is ignored, otherwise highlighted text is replaced
+            setTimeout(function() { reselectField(_activeElement); }, 1);
         }
     }
+}
+function reselectField(_field) {
+    _field.focus();
 }
